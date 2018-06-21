3d915779ec3a0104572dba80ce92990f8e3ea497
MinhTTNguyen <32077384+MinhTTNguyen@users.noreply.github.com>
2018-06-21 14:58:16 -0400

    get list of protein ids grouped into CLAN clusters

--- CLAN/get_CLAN_cluster_list.pl
+++ CLAN/get_CLAN_cluster_list.pl
@@ -5,7 +5,9 @@
 use strict;
 
 my $CLAN_cluster_folder="/home/mnguyen/Research/Symbiomics/AA3/AA3_2/AA3_2_manuscript/CLAN/CLAN_all_clusters";
-my $fileout="/home/mnguyen/Research/Symbiomics/AA3/AA3_2/AA3_2_manuscript/CLAN/CLAN_all_clusters";
+my $fileout="/home/mnguyen/Research/Symbiomics/AA3/AA3_2/AA3_2_manuscript/CLAN/CLAN_all_clusters.txt";
+my $folderout="/home/mnguyen/Research/Symbiomics/AA3/AA3_2/AA3_2_manuscript/CLAN/CLAN_all_clusters_newid";
+mkdir $folderout;
 
 open(Out,">$fileout") || die "Cannot open file $fileout";
 print Out "#Cluster\tID\tDomain_location\n";
@@ -17,40 +19,39 @@ foreach my $file (@files)
 {
 	if (($file ne ".") and ($file ne ".."))
 	{
-		open(In,"<$CLAN_cluster_folder/$file") || die "Cannot open file $file";
+		open(In,"<$CLAN_cluster_folder/$file") || die "Cannot open file $CLAN_cluster_folder/$file";
+		open(FASTA_Out,">$folderout/$file") || die "Cannot open file $folderout/$file";
 		my $cluster=$file;
 		$cluster=~s/^.+\_//;
+		$cluster="C".$cluster;
+		my $id_line="";
+		my $seq="";
 		while (<In>)
 		{
 			$_=~s/\s*$//;
 			if ($_=~/^\>/)
 			{
-				my $id_line=$_;
+				if ($seq)
+				{
+					my $newid=$cluster."|".$id_line;
+					print FASTA_Out ">$newid\n$seq\n";
+					$id_line="";
+					$seq="";
+				}
+				$id_line=$_;
 				$id_line=~s/^\>//;
 				$id_line=~s/\s+\d+$//;
-				if ($id_line=~/\;/)
-				{
-					my @ids=split(/\;/,$id_line);
-					foreach my $each_id (@ids)
-					{
-						my @id_domain=split(/\|/,$each_id);
-						my $seq_id=$id_domain[0];
-						my $domain_loc=$id_domain[1];
-						$domain_loc=~s/\-/../;
-						print Out "$cluster\t$seq_id\t$domain_loc\n";
-					}
-				}
-				else
-				{
-					my @id_domain=split(/\|/,$id_line);
-					my $seq_id=$id_domain[0];
-					my $domain_loc=$id_domain[1];
-					$domain_loc=~s/\-/../;
-					print Out "$cluster\t$seq_id\t$domain_loc\n";
-				}
-			}
+
+				my @id_domain=split(/\|/,$id_line);
+				my $domain_loc=pop(@id_domain);
+				$domain_loc=~s/\-/../;
+				print Out "$cluster\t$id_line\t$domain_loc\n";
+			}else{$_=~s/\s*$//;$seq=$seq.$_;}
 		}
+		my $newid=$cluster."|".$id_line;
+		print FASTA_Out ">$newid\n$seq\n";
 		close(In);
+		close(FASTA_Out);
 	}
 }
 
